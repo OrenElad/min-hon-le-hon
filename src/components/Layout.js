9be98bc47edc3f500656764d8658fea485de7b27
Oren Elad <oren.elad@livepopuli.com>
2016-04-10 21:30:01 +0300

    added basic structure

---
+++ src/components/Layout.js
@@ -0,0 +1,20 @@
+/**
+ * Created by oren on 4/10/16.
+ */
+import React from "react";
+export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "Welcome"
+        };
+    }
+
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ var path = require('path');
 module.exports = {
     context: path.join(__dirname, "src"),
     devtool: debug ? "inline-sourcemap" : null,
-    entry: "./js/client.js",
+    entry: "./app.js",
     module: {
         loaders: [
             {
@@ -24,7 +24,7 @@ module.exports = {
     },
     output: {
         path: __dirname + "/src/",
-        filename: "client.min.js"
+        filename: "app.js"
     },
     plugins: debug ? [] : [
         new webpack.optimize.DedupePlugin(),
