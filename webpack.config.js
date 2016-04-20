01f49a6d223471d416c170d96505e29b4c57b9fa
oren <elador1@gmail.com>
2016-04-20 18:58:22 +0300

    fix some issues

--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,25 @@ var path = require('path');
 module.exports = {
     context: path.join(__dirname, "src"),
     devtool: debug ? "inline-sourcemap" : null,
-    entry: "./app.js",
+    entry: [
+        "./app.js"
+        ],
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
                     presets: ['react', 'es2015', 'stage-0'],
-                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
                 }
             }
         ]
     },
     output: {
-        path: __dirname + "/src/",
-        filename: "app.js"
+        path: __dirname + "/dist/",
+        filename: "app.min.js"
     },
     plugins: debug ? [] : [
         new webpack.optimize.DedupePlugin(),
